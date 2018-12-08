5f875ba7e19686a0e8e6e5c4a9b75fb96eb76e09
Leonardo Di Donato <leodidonato@gmail.com>
2018-12-08 02:31:53 +0100

    Fix: Reading by delimiter updates the main data window too

--- arbitrary_reader.go
+++ arbitrary_reader.go
@@ -8,46 +8,54 @@ import (
 // ArbitraryReader returns a Reader that reads from r
 // but stops when it finds a delimiter.
 // The underlying implementation is a *DelimitedReader.
-func ArbitraryReader(r io.Reader, delimiter byte) Reader {
+func ArbitraryReader(r io.Reader) *DelimitedReader {
 	return &DelimitedReader{
-		reader:    bufio.NewReader(r),
-		delimiter: delimiter,
+		reader: bufio.NewReader(r),
+		parsingState: parsingState{
+			data: []byte{},
+			p:    0,
+			pe:   0,
+			eof:  -1,
+		},
 	}
 }
 
 // DelimitedReader reads arbitrarily sized bytes slices until a delimiter is found.
 // It depends on and it keeps track of Ragel's state variables.
 type DelimitedReader struct {
-	reader    *bufio.Reader
-	delimiter byte
+	reader *bufio.Reader
 
 	parsingState
 }
 
-// Read reads an arbitrarily sized chunk of bytes until it finds a delimiter.
-func (r *DelimitedReader) Read() (n int, err error) {
+// Read reads a chunk of bytes until it finds a delimiter.
+//
+// It always works on the current boundaries of the data,
+// and updates them accordingly.
+// It returns the number of bytes read and, eventually, an error.
+// When delim is not found it returns an io.ErrUnexpectedEOF.
+func (r *DelimitedReader) Read(delim byte) (n int, err error) {
 	p := r.p
-	data := r.data
 
 	// Process only the data still to read when P is greater than the half of the data
 	// data = a b c d e f g h i l m n
 	// vars = - - - - - - - p - pe- -
-	if p > len(data)/2 {
-		copy(data, data[p:len(data)])
+	if p > len(r.data)/2 {
+		copy(r.data, r.data[p:len(r.data)])
 		r.p = 0
 		r.pe = r.pe - p
 		// data = h i l m n f g h i l m n
 		// vars = p - pe- - - - - - - - -
-		data = data[0 : len(data)-p]
+		r.data = r.data[0 : len(r.data)-p]
 		// data = h i l m n
 		// vars = p - pe- -
 	}
 
 	// Read until the first occurrence of the delimiter
-	line, err := r.reader.ReadBytes(r.delimiter)
+	line, err := r.reader.ReadBytes(delim)
 
 	// Storing the data up to and including the delimiter
-	r.data = append(data, line...)
+	r.data = append(r.data, line...)
 	// Update the position of end
 	r.pe = len(r.data)
 	if err == io.EOF {
@@ -58,5 +66,5 @@ func (r *DelimitedReader) Read() (n int, err error) {
 		r.eof = r.pe
 	}
 
-	return n, err
+	return len(line), err
 }
