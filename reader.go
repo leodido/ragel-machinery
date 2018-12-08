09eb2cb380e431c1fd86cf099dccacd35bf696f5
Leonardo Di Donato <leodidonato@gmail.com>
2018-12-08 02:30:37 +0100

    New: Seeker interface

--- reader.go
+++ reader.go
@@ -3,5 +3,11 @@ package ragelmachinery
 // Reader is the interface that wraps the Read method.
 // Its implementations are intended to be used with Ragel parsers or scanners.
 type Reader interface {
-	Read() (n int, err error)
+	Read(until byte) (n int, err error)
+}
+
+// Seeker is the interface that wraps the Seek method.
+// Its implementations are intended to be used with Ragel parsers or scanners.
+type Seeker interface {
+	Seek(until byte, backward bool) (n int, err error)
 }
