5ab439f190bd7d5dfd506abe80b990972161c47a
Leonardo Di Donato <leodidonato@gmail.com>
2018-12-10 15:48:16 +0100

    New: Multiline example

--- examples/example_test.go
+++ examples/example_test.go
@@ -6,20 +6,39 @@ import (
 )
 
 func Example_newlines() {
-	multiline := `example 0
+	in := `example 0
 example 1
 example 2
 example X
 `
 
-	results := (&newlinesMachine{}).Parse(strings.NewReader(multiline))
+	results := (&newlinesMachine{}).Parse(strings.NewReader(in))
 
 	for _, elem := range results {
 		fmt.Println("RECV", elem)
 	}
 	// Output:
 	// OnEOF
+	// OnCompletion
 	// RECV example 0
 	// RECV example 1
 	// RECV example 2
+}
+
+func Example_multiline() {
+	in := `$1
+2
+$3
+`
+	fsm := &multilineMachine{}
+	fsm.Parse(strings.NewReader(in))
+	for _, item := range fsm.items {
+		fmt.Println("RECV", item)
+	}
+	// Output:
+	// OnEOF
+	// OnCompletion
+	// RECV $1
+	// 2
+	// RECV $3
 }
