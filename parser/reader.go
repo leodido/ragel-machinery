dad0fe1553a9a1f17316d27067ec286eb3248136
Leonardo Di Donato <leodidonato@gmail.com>
2018-12-10 11:37:55 +0100

    Update: Readers must be able to return a pointer to the state

--- parser/reader.go
+++ parser/reader.go
@@ -1,9 +1,10 @@
-package ragelmachinery
+package parser
 
 // Reader is the interface that wraps the Read method.
 // Its implementations are intended to be used with Ragel parsers or scanners.
 type Reader interface {
-	Read(until byte) (n int, err error)
+	Read() (n int, err error)
+	State() *State
 }
 
 // Seeker is the interface that wraps the Seek method.
--- state.go
+++
@@ -1,6 +0,0 @@
-package ragelmachinery
-
-type parsingState struct {
-	cs, p, pe, eof int    // standard ragel parsing variables
-	data           []byte // standard ragel data pointer
-}
